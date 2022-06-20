4ff7a06c4756d1c7bb96eb7896c86849be9817d8
Sandeep Kumar <ashagari.sandeep7@gmail.com>
2022-06-20 19:44:52 +0530

    jenkins update codefile

--- code/wp-config.php
+++ code/wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_USER', 'root' );
 /** MySQL database password */
 define( 'DB_PASSWORD', 'Jenkinsmysql@123' );
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '10.128.0.3' );
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
 /** The database collate type. Don't change this if in doubt. */
