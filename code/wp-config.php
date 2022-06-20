d4b2c8daa6ade51a2816bd6969dfc5a661b04aef
Sandeep Kumar <ashagari.sandeep7@gmail.com>
2022-06-20 18:51:30 +0530

    update wp-conigfile

--- code/wp-config.php
+++ code/wp-config.php
@@ -20,11 +20,11 @@ define('WP_CACHE', true); // WP-Optimize Cache
  */
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'kwshh_dev' );
+define( 'DB_NAME', 'kwshh_gcp' );
 /** MySQL database username */
 define( 'DB_USER', 'root' );
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'Jenkinsmysql@123' );
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
 /** Database charset to use in creating database tables. */
